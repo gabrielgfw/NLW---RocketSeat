385166894c2b1a7fbfc00377aa021d3f2bb062bb
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-06-02 15:17:55 -0300

    test return in json on the users route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,20 @@ import express from 'express';
 
 const app = express();
 // --------------------------------------------------------------- //
+// Criando uma rota GET 'localhost:3333/users'                     //
+//                                                                 //
 // request = utilizado para obter dados sobre a requisição.        //
 // response = devolver uma resposta para o navegador ou aplicação. //
 // --------------------------------------------------------------- //
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
 
-    response.send('Hello World');
+    // Retorno definido em formato JSON. //
+    response.json([
+        'Gabriel',
+        'Talita',
+        'Brutus'
+    ]);
 });
-
+// porta 3333 //
 app.listen(3333);
