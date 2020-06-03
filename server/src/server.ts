b464838adf96e39240706104d09bc9db49558a0f
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-06-03 11:12:07 -0300

    post method  test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,36 @@
 import express from 'express';
 
 const app = express();
-// --------------------------------------------------------------- //
-// Criando uma rota GET 'localhost:3333/users'                     //
-//                                                                 //
-// request = utilizado para obter dados sobre a requisição.        //
-// response = devolver uma resposta para o navegador ou aplicação. //
-// --------------------------------------------------------------- //
+
+// -------------------------------------------------------------- //
+// Criando uma rota GET 'localhost:3333/users'                    //
+//                                                                //
+// request  = Utilizado para obter dados sobre a requisição.      //
+// response = Devolve uma resposta para o navegador ou aplicação. //
+// rota     = Endereço completo da requisição 'url'.              //
+// recurso  = Qual endidade estamos acessando do sistema.         //
+// -------------------------------------------------------------- //
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
 
     // Retorno definido em formato JSON. //
-    response.json([
+    return response.json([
         'Gabriel',
         'Talita',
         'Brutus',
         'Gata'
     ]);
 });
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Gabriel',
+        email: 'gabriel@email.com'
+    };
+
+    return response.json(user);
+})
+
+
 // porta 3333 //
 app.listen(3333);
