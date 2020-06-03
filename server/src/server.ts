07e7d403eb9c9cf24c71c470bb44f96304e94500
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-06-03 11:38:07 -0300

    add GET user by id route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,25 +3,39 @@ import express from 'express';
 const app = express();
 
 // -------------------------------------------------------------- //
-// Criando uma rota GET 'localhost:3333/users'                    //
+// Criando rotas 'localhost:3333/users'.                          //
 //                                                                //
 // request  = Utilizado para obter dados sobre a requisição.      //
 // response = Devolve uma resposta para o navegador ou aplicação. //
 // rota     = Endereço completo da requisição 'url'.              //
 // recurso  = Qual endidade estamos acessando do sistema.         //
 // -------------------------------------------------------------- //
+
+const users = [
+    'Gabriel',
+    'Talita',
+    'Brutus',
+    'Gata'
+]
+
+// Rota GET resgatando todos os usuários.
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
 
-    // Retorno definido em formato JSON. //
-    return response.json([
-        'Gabriel',
-        'Talita',
-        'Brutus',
-        'Gata'
-    ]);
+    // Retorno definido em formato JSON.
+    return response.json(users);
 });
 
+// Rota GET para resgatar um usuário pelo ID.
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+
+});
+
+// Rota POST teste.
 app.post('/users', (request, response) => {
     const user = {
         name: 'Gabriel',
