741cf3b0a3440abf30051e2828c9f5ce0b8c9c1f
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-06-05 16:15:22 -0300

    add get route /items to get all items images

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import knex from './database/connection';
 
 // Permite utilizar as rotas desacopladas ao arquivo do servidor.
 const routes = express.Router();
 
-// Rota GET resgatando todos os usuários.
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World'});
+// Rota GET para resgatar todas as imagens dos itens.
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    // Serialização de dados = transforma os dados para um novo formato
+    // que atenda as necessidades do usuário.
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: 'http://localhost:3333/uploads/' + item.image,
+        };
+    });
+
+
+
+    return response.json(serializedItems);
 });
 
 // Permitindo que o server importe as rotas;
