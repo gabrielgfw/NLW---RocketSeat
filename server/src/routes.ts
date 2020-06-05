5a857f44227d4efd185dbc56b46993c7f2550cd0
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-06-05 17:02:11 -0300

    add registry on point_items table when a new point is created

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -41,7 +41,7 @@ routes.post('/points', async (request, response) => {
         items
     } = request.body;
 
-    await knex('points').insert({
+    const insertedIds = await knex('points').insert({
         image: 'image-fake',
         name,
         email,
@@ -51,6 +51,17 @@ routes.post('/points', async (request, response) => {
         city,
         uf
     });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id
+        };
+    });
+
+    await knex('point_items').insert(pointItems);
 
     return response.json({ success: true });
 });
