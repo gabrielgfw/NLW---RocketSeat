b8a755284339cf982ba825c47f1975680ecf018f
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-06-05 16:03:21 -0300

    add route to get images files, populate items table

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -3,6 +3,7 @@ import path from 'path';
 // Possui algumas configurações a mais do que as configurações do banco;
 // Mesmo sendo um arquivo typescript, knex não suporta a sintaxe mais simples;
 module.exports = {
+
     client: 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
@@ -11,5 +12,10 @@ module.exports = {
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
     },
+
+    seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    },
+
     useNullAsDefault: true,
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
@@ -6,6 +7,11 @@ const app = express();
 // Adicionando a extensão express para compreender arquivos do tipo JSON.
 app.use(express.json());
 app.use(routes);
+
+// Adicionando rota para acesso às imagens.
+// Express.static = Serve para disponibilizar arquivos que o cliente necessite
+// baixar para a utilização da aplicação, como imagens por exemplo.
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 // porta 3333 //
 app.listen(3333);
