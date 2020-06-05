131ecac0777aeec77d53cef000cbe2d2f3e57395
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2020-06-05 11:54:24 -0300

    create knexfile to exports modules

---
+++ server/knexfile.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+
+// Possui algumas configurações a mais do que as configurações do banco;
+// Mesmo sendo um arquivo typescript, knex não suporta a sintaxe mais simples;
+module.exports = {
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+    },
+
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    }
+};
